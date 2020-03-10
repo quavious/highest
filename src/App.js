3894d1186e5f4b7cbffc2661fa1129a3f200eac1
quavious <leenowon97@gmail.com>
2020-03-11 03:07:00 +0900

    init react page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ShowInfo from './showInfo'
+import { renderIntoDocument } from 'react-dom/test-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    infos : []
+  };
+  componentDidMount = async() => {
+    const infos = await require('./information/infos.json')
+    this.setState({isLoading: false, infos : infos})
+    
+  }
+
+  render = () => {
+    const {isLoading, infos} = this.state;
+    if (isLoading) {
+      console.log("contents is being loaded...")
+    }
+    else {
+      console.log(infos)
+    }
+    return (
+      <div className="App">
+        <div className="App-title">
+          <h1>the highest</h1>
+        </div>
+        <div className="App-wrapper">
+          {
+            infos.map(info => (
+              <ShowInfo 
+                key = {info.idx}
+                index = {info.idx} 
+                id = {info.id}
+                mainUrl = {info.main_url}
+                imgUrl = {info.imgUrl}
+                numbers = {info.subscribers}
+              />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/showInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './showInfo.css'
+
+function Showinfo({index, id , mainUrl , imgUrl , numbers}) {
+    
+    return (
+        <div className="item-wrapper">
+            <div className="make_radius">
+                <img className ="item-wrapper-img" src={imgUrl} alt={"image"+index} title={"instagram cover image"+{index}} />
+            </div>
+            <a href={mainUrl}><span>{id}</span></a>
+            <h3>{numbers+" "+"Followers"}</h3>
+        </div>
+    )
+}
+
+export default Showinfo
